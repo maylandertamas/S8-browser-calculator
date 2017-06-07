c93a536c3cafecc3c8964555b509cf444a06a388
maylandertamas <maylandertamas@gmail.com>
2017-06-07 19:52:40 +0200

    Added decimal.

--- myScript.js
+++ myScript.js
@@ -1,35 +1,38 @@
 function main () {
 
     var operationString = ""
+
   $(".keys .numButton").on('click', function() {
     var keyValue = $(this).text();
       $(".screen").append(keyValue);
       operationString += keyValue;
     });
-  $(".keys .operator").on('click', function() {
-    if (operationString.length > 0) {
-      alert(operationString.length);
-    var operatorValue = $(this).text();
+  
+  $(".keys .operator").one('click', function() {
+      var operatorValue = $(this).text();
       $(".screen").append(operatorValue);
       operationString += operatorValue;
-      }
     });
+
       $(".keys .eval").on('click', function() {
-        if(operationString !== "" || operationString !== undefined) {
           var replacedDivide = operationString.replace(/÷/g, "/");
           var replacedMultiply = replacedDivide.replace(/x/g, "*");
           var evaledString = eval(replacedMultiply);
           $(".screen").empty();
           $(".screen").append(evaledString);
           operationString = evaledString;
-        }
     });
 
       $(".top .clear").on('click', function() {
         $(".screen").empty();
         operationString = "";
+      });
 
-    });
+      $(".keys .decimal").on('click', function() {
+        var decimalValue = $(this).text();
+        $(".screen").append(decimalValue);
+        operationString += decimalValue;
+      });
  
 }
 $(document).ready(main);
