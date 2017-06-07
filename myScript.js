177cd59371fb548d779e4b1448533fc38ec30c95
maylandertamas <maylandertamas@gmail.com>
2017-06-07 14:21:18 +0200

    Added switch and main functions.

--- myScript.js
+++ myScript.js
@@ -4,37 +4,60 @@
 
 } */
 function main () {
-var final = "";
-var operator = "";
-running = true
-    if ($(".keys .numButton").data('clicked')) )
-        $(".keys .numButton").on('click', function() {
-        var value = $(this).text();
-        $( ".screen" ).append(value);
-        final += value;
-        /*
-        switch ($(".keys .operators").text()) {
-          case "-":
-            code block
-            break;
-          case "+":
-            code block
-            break;
-          case "X":
-            code block
-            break;
-          case "÷":
-            code block
-            break;
-          default:
-            alert("invalid");
-} */ alert("Lol");
-    } else if ($(".keys .operators").data('clicked')) ) {
-        $( ".keys .operators" ).click(function() {
-        $( ".screen" ).empty();});
-        $(".screen").empty()
-    });
-}
+  var num1 = "";
+  var num_array = [];
+  var final = 0;
+  var operator = "";
+  alert(3-2);
+  $(".keys .numButton").on('click', function() {
+      var value = $(this).text();
+      $( ".screen" ).append(value);
+      num_array.push(parseFloat(value));
+      value = ""
+      });
+      $(".keys .operator").on('click', function() {
+      $( ".screen" ).empty();
+      alert($(".keys .operators").text());
+       switch ($(".keys .operators").text()) {
+        case "-":
+          num_array[3] = num_array[0] - num_array[1];
+          alert(num_array);
+          final += num_array[3]
+          num_array.splice(0, 3);
+          break;
+        case "+":
+          num_array[3] = num_array[0] + num_array[1];
+          final += num_array[3]
+          num_array.splice(0, 1);
+          break;
+        case "X":
+          num_array[3] = num_array[0] * num_array[1];
+          final += num_array[3]
+          num_array.splice(0, 1);
+          break;
+        case "÷":
+          num_array[3] = num_array[0] / num_array[1];
+          final += num_array[3]
+          num_array.splice(0, 1);
+          break;
+        }
+      });
+      $(".keys .eval").on('click', function() {
+      $( ".screen" ).append(final)
+      final = 0
+           });
+      $(".keys .clear").on('click', function() {
+        $( ".screen" ).empty()
+        });
+      
+  
+  
+  
+          
+          
+          /*
+          
+  } */ 
 
 
 /* tárolja a számokat stringként, aztán átváltani számmá akkor ha operatorra klikkel
