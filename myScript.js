9ad4b09edcd5b46d89c7db074e78ad140dfa66a9
maylandertamas <maylandertamas@gmail.com>
2017-06-07 13:28:26 +0200

    Started javascript

--- myScript.js
+++ myScript.js
@@ -2,18 +2,44 @@
   $('.keys').hide()
   $('.keys').fadeIn(1000)
 
-}
-$(document).ready(main); */
+} */
 function main () {
-var num1 = "";
+var final = "";
 var operator = "";
-$(".keys .numButton").on('click', function() {
-    var value = $(this).text()
-    num1 += value;)
+running = true
+    if ($(".keys .numButton").data('clicked')) )
+        $(".keys .numButton").on('click', function() {
+        var value = $(this).text();
+        $( ".screen" ).append(value);
+        final += value;
+        /*
+        switch ($(".keys .operators").text()) {
+          case "-":
+            code block
+            break;
+          case "+":
+            code block
+            break;
+          case "X":
+            code block
+            break;
+          case "÷":
+            code block
+            break;
+          default:
+            alert("invalid");
+} */ alert("Lol");
+    } else if ($(".keys .operators").data('clicked')) ) {
+        $( ".keys .operators" ).click(function() {
+        $( ".screen" ).empty();});
+        $(".screen").empty()
+    });
+}
+
+
 /* tárolja a számokat stringként, aztán átváltani számmá akkor ha operatorra klikkel
    ezután másik számot is ugyanúgy majd ha az egyenlőségre kattint akkor kettőt osztani az operatortól függően
    amit szintén kimenteni egy varablebe */
- });
  
 }
 $(document).ready(main);
