74498bb2e011311a6cab48768ed116f929ed630f
maylandertamas <maylandertamas@gmail.com>
2017-06-07 11:58:08 +0200

    Setup code skeletons.

---
+++ myScript.js
@@ -0,0 +1,19 @@
+/* function main () {
+  $('.keys').hide()
+  $('.keys').fadeIn(1000)
+
+}
+$(document).ready(main); */
+function main () {
+var num1 = "";
+var operator = "";
+$(".keys .numButton").on('click', function() {
+    var value = $(this).text()
+    num1 += value;)
+/* tárolja a számokat stringként, aztán átváltani számmá akkor ha operatorra klikkel
+   ezután másik számot is ugyanúgy majd ha az egyenlőségre kattint akkor kettőt osztani az operatortól függően
+   amit szintén kimenteni egy varablebe */
+ });
+ 
+}
+$(document).ready(main);
